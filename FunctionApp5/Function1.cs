054e0e85fafb860a47052bfa71357a63e389c160
UagNanit <Olegkrava7@gmail.com>
2023-03-21 02:50:22 +0200

    f

--- FunctionApp5/Function1.cs
+++ FunctionApp5/Function1.cs
@@ -1,54 +1,67 @@
 using System;
 using System.IO;
+using System.Reflection.Metadata;
 using System.Threading.Tasks;
 using FunctionApp5.Data;
 using FunctionApp5.Repository;
+using FunctionApp5.Services;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Protocols;
 
 namespace FunctionApp5
 {
     public class Function1
     {
+        
        
         [FunctionName("Function1")]
         public async Task RunAsync([BlobTrigger("files-container/{name}.{extension}", Connection = "BlobConnectionString")] Stream myBlob, Uri uri, string name, string extension, ILogger log)
         {
-            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes\n {Environment.GetEnvironmentVariable("DefaultConnection")}");
-
-            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
-
-            var _emailSender = new EmailSender();
-
-
-            using (var _applicationContext = new ApplicationContext(Environment.GetEnvironmentVariable("DefaultConnection")))
+            try
             {
-                var obj = _applicationContext.DbModelDatas.FirstOrDefaultAsync(d => d.FileName == name + "." + extension).Result;
+                log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes\nconstr: {Environment.GetEnvironmentVariable("ConnectionStrings:DefaultConnection")} ");
 
-                if (obj != null)
+                var _emailSender = new EmailSender();
+
+                var str = Environment.GetEnvironmentVariable("ConnectionStrings:DefaultConnection") ?? "Server=tcp:reenbittesttaskdbserver.database.windows.net,1433;Initial Catalog=ReenbitTestTask_db;Persist Security Info=False;User ID=nanit;Password=19891206Kov;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+                using (var _applicationContext = new ApplicationContext(str))
                 {
-                    log.LogInformation("\nEmail: " + obj.Email);
+                    var obj = _applicationContext.DbModelDatas.FirstOrDefaultAsync(d => d.FileName == name + "." + extension).Result;
 
-                    string sendTo = obj.Email;
-                    string subject = "Add file to the BLOB storage";
-                    string body = $"You are add file \nName: {name} \nType: {extension} \nSize: {myBlob.Length} Bytes \nUri: {uri?.ToString()}";
-
-                    await _emailSender.SendEmailAsync(sendTo, subject, body);
-                }
-                else
-                {
-                    log.LogInformation("\nEmail: null");
-
-                    string sendTo = "olegkrava7@gmail.com";
-                    string subject = "Add file to the BLOB storage";
-                    string body = $"You are add file \nName: {name} \nType: {extension} \nSize: {myBlob.Length} Bytes \nUri: {uri?.ToString()} \n Email: null";
-
-                    await _emailSender.SendEmailAsync(sendTo, subject, body);
-
+                    if (obj != null)
+                    {
+                        log.LogInformation("\nEmail: " + obj.Email);
+                        string sendTo = obj.Email;
+                        string subject = "Add file to the BLOB storage";
+                        string body = $"You are add file \nName: {name} \nType: {extension} \nSize: {myBlob.Length} Bytes \nUri: {uri?.ToString()}";
+                        await _emailSender.SendEmailAsync(sendTo, subject, body);
+                    }
+                    
                 }
             }
+            catch (Exception ex)
+            {
+                string sendTo = "olegkrava7@gmail.com";
+                string subject = "Exception";
+                string body = $"You are add file \nName: {name} \nType: {extension} \nSize: {myBlob.Length} Bytes \nUri: {uri?.ToString()} \n Email: null";
+
+                await new EmailSender().SendEmailAsync(sendTo, subject, body);
+                log.LogInformation($"Exception: {ex.Message}");
+            }
+            finally
+            {
+                string sendTo = "olegkrava7@gmail.com";
+                string subject = "finally";
+                string body = $"You are add file \nName: {name} \nType: {extension} \nSize: {myBlob.Length} Bytes \nUri: {uri?.ToString()} \n Email: null";
+
+                await new EmailSender().SendEmailAsync(sendTo, subject, body);
+            }
+           
+            
+            
         }
     }
 }
